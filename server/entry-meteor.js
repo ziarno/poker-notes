dbd1c0231366b2bc696e09b210fdd5f5952cba49
Filip Jarno <filip@jarno.pl>
2026-01-04 10:31:48 +0100

    init with meteor and vue

---
+++ server/entry-meteor.js
@@ -0,0 +1,21 @@
+/**
+ * These modules are automatically imported by jorgenvatle:vite.
+ * You can commit these to your project or move them elsewhere if you'd like,
+ * but they must be imported somewhere in your Meteor mainModule.
+ *
+ * More info: https://github.com/JorgenVatle/meteor-vite#lazy-loaded-meteor-packages
+ **/
+import "../_vite-bundle/server/_entry.mjs"
+/** End of vite auto-imports **/
+/**
+ * Entrypoint for the Meteor server
+ * Generally, this file can be left empty. Vite will add imports for your app's
+ * server bundle here during both development and production build.
+ *
+ * Use ./main.js as the primary entrypoint for your app to take full advantage
+ * of Vite's plugin and build system.
+ *
+ * This can also be a good place to put code that you don't want Vite to
+ * process, for example, if you run into a compatibility issue or need to use
+ * nested imports.
+ */
