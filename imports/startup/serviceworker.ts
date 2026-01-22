2838f5557a3a4aa25704ff10d15081a313baca32
Filip Jarno <filip@jarno.pl>
2026-01-22 11:17:22 +0100

    use service worker only in production

--- imports/startup/serviceworker.ts
+++ imports/startup/serviceworker.ts
@@ -1,6 +1,8 @@
 Meteor.startup(async () => {
   try {
-    await navigator.serviceWorker.register('/sw.js')
+    if (Meteor.isProduction) {
+      await navigator.serviceWorker.register('/sw.js')
+    }
   } catch (error) {
     console.error('Service Worker registration failed:', error)
   }
