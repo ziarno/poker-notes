91be7d4727bfe35fe8d03e568599e9238b1a74a4
Filip Jarno <filip@jarno.pl>
2026-01-22 19:31:58 +0100

    add .keep to GamesCollection

--- imports/api/collections/games.collection.ts
+++ imports/api/collections/games.collection.ts
@@ -3,3 +3,6 @@ import { Mongo } from 'meteor/mongo'
 import { Game } from '@/types/Game.type'
 
 export const GamesCollection = new Mongo.Collection<Game>('games')
+
+// Keep games for offline access
+GamesCollection.keep({}, { limit: 100, sort: { createdAt: -1 } })
