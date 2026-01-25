ac7b595b40668c29e6896a51e8a8034bcf88dc7c
Filip Jarno <filip@jarno.pl>
2026-01-26 00:17:58 +0100

    add nowrap in Balance.vue

--- imports/ui/components/Balance.vue
+++ imports/ui/components/Balance.vue
@@ -7,5 +7,7 @@ const colorClass = !value ? '' : value > 0 ? 'text-lime-600' : 'text-red-700'
 </script>
 
 <template>
-  <span :class="`font-bold ${colorClass}`">{{ balanceToString(value) }}</span>
+  <span :class="`font-bold text-nowrap ${colorClass}`">{{
+    balanceToString(value)
+  }}</span>
 </template>
