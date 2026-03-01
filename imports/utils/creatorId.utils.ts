77cd5b3492b5075c3d6b6c5093473d66a1af3077
Filip Jarno <filip@jarno.pl>
2026-03-01 19:20:07 +0100

    replace crypto with Random

--- imports/utils/creatorId.utils.ts
+++ imports/utils/creatorId.utils.ts
@@ -1,9 +1,11 @@
+import { Random } from 'meteor/random'
+
 const CREATOR_ID_KEY = 'creatorId'
 
 export function getCreatorId(): string {
   let id = localStorage.getItem(CREATOR_ID_KEY)
   if (!id) {
-    id = crypto.randomUUID()
+    id = Random.id()
     localStorage.setItem(CREATOR_ID_KEY, id)
   }
   return id
